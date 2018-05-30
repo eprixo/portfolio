3deabeebea905f79827669d3d8040fd7ed13ef1c
Kashif Ford <kashif.ford@gmail.com>
2018-05-30 11:42:36 -0500

    new contact form

--- contact-form.php
+++ contact-form.php
@@ -18,7 +18,7 @@ if (isset($_POST['inputName']) && isset($_POST['inputEmail'])&& isset($_POST['in
     $mail->From = $_POST['inputEmail'];
     $mail->FromName = $_POST['inputName'];
     $mail->AddAddress('kashif.ford@gmail.com'); //recipient 
-    $mail->Subject = 'Enquiry from Website';
+    $mail->Subject = 'Enquiry from Eprixo';
     $mail->Body = "Name: " . $_POST['inputName'] . "\r\n\r\nMessage: " . stripslashes($_POST['inputMessage']);
 
     if (isset($_POST['ref'])) {
